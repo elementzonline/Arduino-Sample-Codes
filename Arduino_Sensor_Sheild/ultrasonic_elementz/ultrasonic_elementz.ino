6908fbbdd33384adb45856620daa015baa192360
elementzonline <dhanish@elementzonline.com>
2015-03-26 18:59:19 +0530

    By Dhnish

--- Arduino_Sensor_Sheild/ultrasonic_elementz/ultrasonic_elementz.ino
+++ Arduino_Sensor_Sheild/ultrasonic_elementz/ultrasonic_elementz.ino
@@ -43,20 +43,11 @@ void loop() {
  
  //Calculate the distance (in cm) based on the speed of sound.
  distance = duration/58.2;
- //Serial.println(duration);
- if (distance >= maximumRange || distance <= minimumRange){
- /* Send a negative number to computer and Turn LED ON 
- to indicate "out of range" */
- Serial.println("-1");
- digitalWrite(LEDPin, HIGH); 
- }
- else {
- /* Send the distance to the computer using Serial protocol, and
- turn LED OFF to indicate successful reading. */
+ 
  Serial.println(distance);
  digitalWrite(LEDPin, LOW); 
- }
+
  
  //Delay 50ms before next reading.
- delay(50);
+ delay(100);
 }
