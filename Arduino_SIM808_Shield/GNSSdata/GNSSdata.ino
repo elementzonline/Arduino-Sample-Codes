10e6c05bd2b07c95c33c8789ce863ccb85c99436
dhanishvijayan <dhaniishvijayan@gmail.com>
2016-11-28 16:26:59 +0530

    Added Sim808 arduino shield test codes

---
+++ Arduino_SIM808_Shield/GNSSdata/GNSSdata.ino
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+ #include "ElementzGSM.h"
+
+SoftwareSerial myserialOut(6,7);
+ElementzGSMshield GSM(&myserialOut);
+String testString = "";
+void setup() {
+  
+  Serial.begin(9600);
+  GSM.begin(9600);
+  Serial.println("Starting");
+  delay(1000);
+  GSM.turnOnGNSS();
+  String testString = GSM.getGNSSInfo();
+  Serial.println(testString);
+  GSM.turnOffGNSS();
+
+}
+
+void loop() {
+
+}
---
+++ Arduino_SIM808_Shield/SMSsending/SMSsending.ino
@@ -0,0 +1,32 @@
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+ #include "ElementzGSM.h"
+
+
+SoftwareSerial myserialOut(6,7);
+ElementzGSMshield GSM(&myserialOut);
+String testString = "";
+void setup() {
+  Serial.begin(9600);
+   GSM.begin(9600);
+  Serial.println("Starting");
+  delay(1000);
+  
+   boolean sms_status = GSM.sendSms("7356xxxxxx", "Test Message");//you can input the number also as a String object 
+   if (sms_status == true)
+   {
+     Serial.println("SMS Send");
+   }
+   else
+   {
+     Serial.println("SMS sending Failed");
+   }
+ 
+
+  
+
+}
+
+void loop() {
+
+}
