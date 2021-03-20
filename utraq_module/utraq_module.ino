9695edf2445454a9c36d22b9ebcf9af31200846e
saranyapu <62689256+saranyapu@users.noreply.github.com>
2021-03-20 16:48:15 +0530

    Create utraq_module.ino

---
+++ utraq_module/utraq_module.ino
@@ -0,0 +1,135 @@
+
+/*    Sample Code to check the working of UTRAQ Module: used for vehicle tracking and Geodetic data.
+ *    Company : Elementz Engineers Guild Pvt Ltd
+ *    Website: http://www.elementzonline.com
+ *    Model:U-TRAQ L100 Module
+ *    Product Link: https://www.elementzonline.com/u-traq-l100-irnss-navic-gagan-sbas-egnos-epotm-waas-msas-gnss-glonass-stagnss-l1-l5-nmea-gps-receiver-module
+ *    Basic working of UTRAQ Module: https://www.elementzonline.com/blog/utraq-gps-l100-module-based-on-irnss-supported-by-gagan-navic
+ 
+
+ *  Add Library
+ *  Here we used TinyGPS library 
+ */
+
+#include <TinyGPS.h>
+TinyGPS gps;
+
+void gpsdump(TinyGPS &gps);
+void printFloat(double f, int digits = 2);
+
+void setup()
+{
+  Serial.begin(115200);          // initialize serial communications:
+  delay(1000);
+  Serial.println();
+  Serial.print("Sizeof(gpsobject) = "); Serial.println(sizeof(TinyGPS));
+  Serial.println();
+}
+
+void loop()
+{
+  bool newdata = false;
+  unsigned long start = millis();
+
+  // Every 5 seconds we print an update
+  while (millis() - start < 5000) {
+    if (Serial.available()) {
+      char c = Serial.read();
+      // Serial.print(c);     // uncomment to see raw GPS data
+      if (gps.encode(c)) {
+        newdata = true;
+        // break;            // uncomment to print new data immediately!
+      }
+    }
+  }
+  
+  if (newdata) {
+    Serial.println("Acquired Data");
+    Serial.println("-------------");
+    gpsdump(gps);
+    Serial.println("-------------");
+    Serial.println();
+  }
+}
+
+void gpsdump(TinyGPS &gps)
+{
+  long lat, lon;
+  float flat, flon;
+  unsigned long age, date, time, chars;
+  int year;
+  byte month, day, hour, minute, second, hundredths;
+  unsigned short sentences, failed;
+
+  gps.get_position(&lat, &lon, &age);
+  Serial.print("Lat/Long(10^-5 deg): "); Serial.print(lat); Serial.print(", "); Serial.print(lon); 
+  Serial.print(" Fix age: "); Serial.print(age); Serial.println("ms.");
+  
+  // On Arduino, GPS characters may be lost during lengthy Serial.print()
+  // On Teensy, Serial prints to USB, which has large output buffering and
+  //   runs very fast, so it's not necessary to worry about missing 4800
+  //   baud GPS characters.
+
+  gps.f_get_position(&flat, &flon, &age);
+  Serial.print("Lat/Long(float): "); printFloat(flat, 5); Serial.print(", "); printFloat(flon, 5);
+  Serial.print(" Fix age: "); Serial.print(age); Serial.println("ms.");
+
+  gps.get_datetime(&date, &time, &age);
+  Serial.print("Date(ddmmyy): "); Serial.print(date); Serial.print(" Time(hhmmsscc): ");
+    Serial.print(time);
+  Serial.print(" Fix age: "); Serial.print(age); Serial.println("ms.");
+
+  gps.crack_datetime(&year, &month, &day, &hour, &minute, &second, &hundredths, &age);
+  Serial.print("Date: "); Serial.print(static_cast<int>(month)); Serial.print("/"); 
+    Serial.print(static_cast<int>(day)); Serial.print("/"); Serial.print(year);
+  Serial.print("  Time: "); Serial.print(static_cast<int>(hour)); Serial.print(":"); 
+    Serial.print(static_cast<int>(minute)); Serial.print(":"); Serial.print(static_cast<int>(second));
+    Serial.print("."); Serial.print(static_cast<int>(hundredths));
+  Serial.print("  Fix age: ");  Serial.print(age); Serial.println("ms.");
+
+  Serial.print("Alt(cm): "); Serial.print(gps.altitude()); Serial.print(" Course(10^-2 deg): ");
+    Serial.print(gps.course()); Serial.print(" Speed(10^-2 knots): "); Serial.println(gps.speed());
+  Serial.print("Alt(float): "); printFloat(gps.f_altitude()); Serial.print(" Course(float): ");
+    printFloat(gps.f_course()); Serial.println();
+  Serial.print("Speed(knots): "); printFloat(gps.f_speed_knots()); Serial.print(" (mph): ");
+    printFloat(gps.f_speed_mph());
+  Serial.print(" (mps): "); printFloat(gps.f_speed_mps()); Serial.print(" (kmph): ");
+    printFloat(gps.f_speed_kmph()); Serial.println();
+
+  gps.stats(&chars, &sentences, &failed);
+  Serial.print("Stats: characters: "); Serial.print(chars); Serial.print(" sentences: ");
+    Serial.print(sentences); Serial.print(" failed checksum: "); Serial.println(failed);
+}
+
+void printFloat(double number, int digits)
+{
+  // Handle negative numbers
+  if (number < 0.0) {
+     Serial.print('-');
+     number = -number;
+  }
+
+  // Round correctly so that print(1.999, 2) prints as "2.00"
+  double rounding = 0.5;
+  for (uint8_t i=0; i<digits; ++i)
+    rounding /= 10.0;
+  
+  number += rounding;
+
+  // Extract the integer part of the number and print it
+  unsigned long int_part = (unsigned long)number;
+  double remainder = number - (double)int_part;
+  Serial.print(int_part);
+
+  // Print the decimal point, but only if there are digits beyond
+  if (digits > 0)
+    Serial.print("."); 
+
+  // Extract digits from the remainder one at a time
+  while (digits-- > 0) {
+    remainder *= 10.0;
+    int toPrint = int(remainder);
+    Serial.print(toPrint);
+    remainder -= toPrint;
+  }
+}
